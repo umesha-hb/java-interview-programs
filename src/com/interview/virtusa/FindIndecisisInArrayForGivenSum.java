4fe6972af7500d8b057bc5da33fc25336ab644b9
umesha-hb <umesha.kit@gmail.com>
2026-04-23 12:26:58 +0530

    update

--- src/com/interview/virtusa/FindIndecisisInArrayForGivenSum.java
+++ src/com/interview/virtusa/FindIndecisisInArrayForGivenSum.java
@@ -6,19 +6,18 @@ import java.util.Map;
 public class FindIndecisisInArrayForGivenSum {
     public static void main(String args[])
     {
-        int arr[]={2,1,3,8,7};
-        int sum=5;
+        int arr[]={2,7,11,15};
+        int sum=9;
         Map<Integer,Integer> map = new HashMap<>();
         for(int i =0;i<arr.length;i++)
         {
             int index = sum-arr[i];
             if(map.containsKey(index))
             {
-                System.out.println(i+":"+map.get(i));
+                System.out.println(i+":"+map.get(index));
               break;
             }
             map.put(arr[i],i);
         }
-        System.out.println(map);
     }
 }
